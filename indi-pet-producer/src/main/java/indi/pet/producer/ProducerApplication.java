10cffed614a1414c2ea78f1afdd298c7acc8db2c
韩超 <dajiangyouzzz@outlook.com>
2018-11-01 16:52:16 +0800

    初始化宠物店主系统工程

---
+++ indi-pet-producer/src/main/java/indi/pet/producer/ProducerApplication.java
@@ -0,0 +1,28 @@
+package indi.pet.producer;
+
+import org.elasticsearch.node.NodeBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+@SpringBootApplication
+@EnableElasticsearchRepositories
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Bean
+	public ElasticsearchOperations elasticsearchTemplate(){
+		NodeBuilder nodeBuilder=nodeBuilder();
+		nodeBuilder.settings().put("path.home",System.getenv("ES_HOME"));
+		return new ElasticsearchTemplate(nodeBuilder.local(true).build().client());
+	}
+}
