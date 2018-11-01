c1537f92df2e692466400c07d8bf900e08cae812
韩超 <dajiangyouzzz@outlook.com>
2018-11-01 17:03:07 +0800

    宠物买家系统工程初始化

---
+++ indi-pet-consumer/src/main/java/indi/pet/consumer/ConsumerApplication.java
@@ -0,0 +1,25 @@
+package indi.pet.consumer;
+
+import org.elasticsearch.node.NodeBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@SpringBootApplication
+@EnableElasticsearchRepositories
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@Bean
+	public ElasticsearchOperations elasticsearchTemplate(){
+		NodeBuilder nodeBuilder=NodeBuilder.nodeBuilder();
+		nodeBuilder.settings().put("path.home",System.getenv("ES_HOME"));
+		return new ElasticsearchTemplate(nodeBuilder.local(true).node().client());
+	}
+}
